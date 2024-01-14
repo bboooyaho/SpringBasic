3f9b69efc4e5094bb83f9127ed8a01b5e8824ae0
dain7 <dainnnnnn96@gmail.com>
2024-01-14 13:25:32 +0900

    Third commit

--- main/java/hello/core/order/OrderServiceImpl.java
+++ main/java/hello/core/order/OrderServiceImpl.java
@@ -6,25 +6,27 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository ;
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    //private final DiscountPolicy discountPolicy= new FixDiscountPolicy();
 
-
-
-
+    //생성자가 한 개만 있을 시, Autowired 생략 가능
+    //@Autowired
+    //lombok 이 해결함
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+//
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
 
